c45003044744c215fc4bddc6da147f219d65b95e
syb01094648 <syb01094648@antgroup.com>
2023-10-25 14:27:34 +0800

    fix: 删除无用代码

--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import path from 'path';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  let a = vscode.commands.registerCommand('a', () => {
-    console.log(context);
-    // const panel = vscode.window.createWebviewPanel(
-    //   "React",
-    //   "React App",
-    //   vscode.ViewColumn.One,s
-    //   {
-    //     retainContextWhenHidden: true, // 保证 Webview 所在页面进入后台时不被释放
-    //     enableScripts: true, // 运行 JS 执行
-    //   }
-    // );
+  let editor = vscode.commands.registerCommand('l7-editor', () => {
+    const panel = vscode.window.createWebviewPanel(
+      'React',
+      'React App',
+      vscode.ViewColumn.One,
+      {
+        retainContextWhenHidden: true, // 保证 Webview 所在页面进入后台时不被释放
+        enableScripts: true, // 运行 JS 执行
+      },
+    );
 
-    // const isProduction =
-    //   context.extensionMode === vscode.ExtensionMode.Production;
-    // let srcUrl = "";
-    // if (isProduction) {
-    //   const filePath = vscode.Uri.file(
-    //     path.join(context.extensionPath, "dist", "static/js/main.js")
-    //   );
-    //   srcUrl = panel.webview.asWebviewUri(filePath).toString();
-    // } else {
-    //   srcUrl = "http://localhost:3000/static/js/main.js";
-    // }
-    // panel.webview.html = getWebviewContent(srcUrl);
+    const isProduction =
+      context.extensionMode === vscode.ExtensionMode.Production;
+    let srcUrl = '';
+    if (isProduction) {
+      const filePath = vscode.Uri.file(
+        path.join(context.extensionPath, 'dist', 'static/js/main.js'),
+      );
+      srcUrl = panel.webview.asWebviewUri(filePath).toString();
+    } else {
+      srcUrl = 'http://localhost:3000/static/js/main.js';
+    }
+    panel.webview.html = getWebviewContent(srcUrl);
 
-    // const updateWebview = () => {
-    //   panel.webview.html = getWebviewContent(srcUrl);
-    // };
-    // updateWebview()
-
-    // if (url) {
-    //   // vscode.window.showInformationMessage(url.fsPath);
-    //   fetch(`${url.fsPath}`)
-    //     .then((response) => response.json())
-    //     .then((data) => console.log(data));
-    // } else {
-    //   console.log("No folder is selected.");
-    // }
-    // console.log("aaaa");
+    const updateWebview = () => {
+      panel.webview.html = getWebviewContent(srcUrl);
+    };
+    updateWebview();
   });
 
-  context.subscriptions.push(a);
+  context.subscriptions.push(editor);
 }
 
 // This method is called when your extension is deactivated
